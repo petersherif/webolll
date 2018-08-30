3d6ece5a7bfdc6145967e28e3d4a367cf4624126
Peter Sherif <petershreif@gmail.com>
2018-08-30 22:49:40 +0200

    Design Refining | Add ScrollSpy Effect

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
 
   // Smooth scrolling
-	var scrollLink = $('.smooth-scroll');
-  
-  scrollLink.click(function(e) {
+  $('.smooth-scroll').click(function(e) {
     e.preventDefault();
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top
@@ -17,7 +15,43 @@ $(document).ready(function () {
 
 	// Navbar background on scroll and expand
 	$(document).scroll(function () {
+
+			// Add background color to navbar on scroll > 60px
 			$('.navbar').toggleClass('navbar-bg-scroll', $(this).scrollTop() > 60);
+
+
+			// ScrollSpy
+
+			// Get out nav links
+			var	bodyOffset = 66,
+					links = $('.navbar .nav-link');
+
+			// Loop throught the nav links
+			for (var i = 0; i < links.length; i++) {
+
+				// Get the current link href
+				var link = links[i];
+
+				// Get the link hash
+				var hash = $(link).attr('href');
+
+				// Get the associated element
+				var element = $(hash);
+
+				if (element.length > 0) {
+					var $windowTop = $(window).scrollTop() + bodyOffset,
+							$elementTop = Math.floor($(element).offset().top);
+							$elementBottom = Math.floor($elementTop + $(element).outerHeight());
+							console.log($windowTop);
+
+					if ($windowTop >= $elementTop && $windowTop < $elementBottom) {
+						$(link).parent().siblings().removeClass('active');
+						$(link).parent().addClass('active');
+					} else if($windowTop > $elementBottom) {
+						$(link).parent().removeClass('active');
+					}
+				}
+			}
 	});
 
 	$('.navbar-toggler').click(function() {
